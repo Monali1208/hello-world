ceac1df28ef19dbda3029de8f34bbc756b425276
Monali1208 <89089637+Monali1208@users.noreply.github.com>
2022-01-26 10:50:08 -0500

    Initial commit

---
+++ Iphone.java
@@ -0,0 +1,16 @@
+package oopsconcept;
+
+public class Iphone {
+	
+	public void makecall()
+	
+	{
+		System.out.println("Making call feature developed");
+	}
+	
+	public void receivecall()
+	{
+		System.out.println("Receiving call feature developed");
+	}
+
+}
